667bbe4055df3230377d43c21831e07cf6535b82
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-05-16 17:01:23 -0300

    feat: GitCommitter VoiceAssistant integration. VoiceAssistant now provides an external logic lopp features

--- api/src/domain/GitCommitter.py
+++ api/src/domain/GitCommitter.py
@@ -1,63 +1,8 @@
 import subprocess, codecs
 
-import gitc
+import gitc, WakeUpVoiceAssistant, VoiceAssistant, GitCommand
 
-class Command:
-
-    KW_GIT = 'git'
-    KW_SELF = 'self'
-    KW_CLONE = 'clone'
-    KW_STATUS = 'status'
-    KW_BRANCH = 'branch'
-    KW_PULL = 'pull'
-    KW_CHECKOUT = 'checkout'
-    KW_ADD = 'add'
-    KW_COMMIT = 'commit'
-    KW_PUSH = 'push'
-    KW_FETCH = 'fetch'
-    KW_MERGE = 'merge'
-    KW_ORIGIN = 'origin'
-
-    TOKEN_REPOSITORY_URL = '__TOKEN_REPOSITORY_URL__'
-    TOKEN_COMMIT_MESSAGE = '__TOKEN_COMMIT_MESSAGE__'
-    TOKEN_BRANCH_NAME = '__TOKEN_BRANCH_NAME__'
-
-    CLONE = f'{KW_GIT} {KW_CLONE} {TOKEN_REPOSITORY_URL}'
-    STATUS = f'{KW_GIT} {KW_STATUS}'
-    BRANCH = f'{KW_GIT} {KW_BRANCH}'
-    PULL = f'{KW_GIT} {KW_PULL}'
-    CHECKOUT = f'{KW_GIT} {KW_CHECKOUT} {TOKEN_BRANCH_NAME}'
-    CHECKOUT_DASH_B = f'{KW_GIT} {KW_CHECKOUT} -b {TOKEN_BRANCH_NAME}'
-    ADD = f'{KW_GIT} {KW_ADD} .'
-    COMMIT = f'{KW_GIT} {KW_COMMIT} -m "{TOKEN_COMMIT_MESSAGE}"'
-    PUSH = f'{KW_GIT} {KW_PUSH}'
-    FETCH = f'{KW_GIT} {KW_FETCH}'
-    MERGE = f'{KW_GIT} {KW_MERGE}'
-    ORIGIN = f'{KW_GIT} {KW_ORIGIN}'
-    MERGE_ORIGIN = f'{KW_GIT} {KW_MERGE} {KW_ORIGIN}/{TOKEN_BRANCH_NAME}'
-    PUSH_SET_UPSTREAM_ORIGIN = f'{KW_GIT} {KW_PUSH} --set-upstream {KW_ORIGIN}'
-    PUSH_SET_UPSTREAM_ORIGIN_BRANCH = f'{PUSH_SET_UPSTREAM_ORIGIN} {TOKEN_BRANCH_NAME}'
-
-
-KW_ALL = 'all'
-KW_IF_DASH_NEEDED = 'if-needed'
-
-COMMAND_CLONE_ALL_IF_NEEDED = f'{Command.KW_CLONE}-{KW_ALL}-{KW_IF_DASH_NEEDED}'
-COMMAND_CHECKOUT_B_ALL_IF_NEEDED = f'{Command.KW_CHECKOUT}-b-{KW_ALL}-{KW_IF_DASH_NEEDED}'
-COMMAND_PUSH_SET_UPSTREAM_ORIGIN_BRANCH_IF_NEEDED = f'{Command.KW_PUSH}-set-upstream-{Command.KW_ORIGIN}-{KW_ALL}-{KW_IF_DASH_NEEDED}'
-
-COMMAND_STATUS_ALL = f'{Command.KW_STATUS}-{KW_ALL}'
-COMMAND_BRANCH_ALL = f'{Command.KW_BRANCH}-{KW_ALL}'
-COMMAND_PULL_ALL = f'{Command.KW_PULL}-{KW_ALL}'
-COMMAND_CHECKOUT_ALL = f'{Command.KW_CHECKOUT}-{KW_ALL}'
-COMMAND_ADD_ALL = f'{Command.KW_ADD}-{KW_ALL}'
-COMMAND_COMMIT_ALL = f'{Command.KW_COMMIT}-{KW_ALL}'
-COMMAND_PUSH_ALL = f'{Command.KW_PUSH}-{KW_ALL}'
-COMMAND_ADD_COMMIT_PUSH_ALL = f'{Command.KW_ADD}-{Command.KW_COMMIT}-{Command.KW_PUSH}-{KW_ALL}'
-COMMAND_MERGE_ORIGIN_ALL = f'{Command.KW_MERGE}-{Command.KW_ORIGIN}-{KW_ALL}'
-COMMAND_ADD_ENVIRONMENT_VARIABLE = f'add-environment-variable'
-
-COMMAND_SKIP = 'skip'
+GitCommand = GitCommand.GitCommand
 
 class GitCommitter:
 
@@ -65,6 +10,58 @@ class GitCommitter:
     COMMAND_INDEX = 1
     _1_ARGUMENT_INDEX = 2
     _2_ARGUMENT_INDEX = 3
+
+    MISSING_SPACE = 'Missing '
+
+    WAKE_UP_VOICE_ASSISTANT = 'voice-assistant'
+
+    KW_ALL = 'all'
+    KW_IF_DASH_NEEDED = 'if-needed'
+
+    CLONE_ALL_IF_NEEDED = f'{GitCommand.KW_CLONE}-{KW_ALL}-{KW_IF_DASH_NEEDED}'
+    CHECKOUT_B_ALL_IF_NEEDED = f'{GitCommand.KW_CHECKOUT}-b-{KW_ALL}-{KW_IF_DASH_NEEDED}'
+    PUSH_SET_UPSTREAM_ORIGIN_BRANCH_IF_NEEDED = f'{GitCommand.KW_PUSH}-set-upstream-{GitCommand.KW_ORIGIN}-{KW_ALL}-{KW_IF_DASH_NEEDED}'
+
+    STATUS_ALL = f'{GitCommand.KW_STATUS}-{KW_ALL}'
+    BRANCH_ALL = f'{GitCommand.KW_BRANCH}-{KW_ALL}'
+    PULL_ALL = f'{GitCommand.KW_PULL}-{KW_ALL}'
+    CHECKOUT_ALL = f'{GitCommand.KW_CHECKOUT}-{KW_ALL}'
+    ADD_ALL = f'{GitCommand.KW_ADD}-{KW_ALL}'
+    COMMIT_ALL = f'{GitCommand.KW_COMMIT}-{KW_ALL}'
+    PUSH_ALL = f'{GitCommand.KW_PUSH}-{KW_ALL}'
+    ADD_COMMIT_PUSH_ALL = f'{GitCommand.KW_ADD}-{GitCommand.KW_COMMIT}-{GitCommand.KW_PUSH}-{KW_ALL}'
+    MERGE_ORIGIN_ALL = f'{GitCommand.KW_MERGE}-{GitCommand.KW_ORIGIN}-{KW_ALL}'
+    ADD_ENVIRONMENT_VARIABLE = f'add-environment-variable'
+
+    SKIP = 'skip'
+
+    CONFIRM = ['execute']
+
+    def __init__(self,globals):
+        self.globals = globals
+        self.GIT_COMMITTER = globals.GITC_GIT_COMMITTER
+        self.gitUrl = globals.getApiSetting(f'api.git.url')
+        self.gitExtension = globals.getApiSetting(f'api.git.extension')
+        self.commandSet = {
+            GitCommitter.WAKE_UP_VOICE_ASSISTANT : self.wakeUpVoiceAssistant,
+
+            GitCommitter.CLONE_ALL_IF_NEEDED : self.cloneAllIfNeeded,
+            GitCommitter.CHECKOUT_B_ALL_IF_NEEDED : self.checkoutBAllIfNeeded,
+            GitCommitter.PUSH_SET_UPSTREAM_ORIGIN_BRANCH_IF_NEEDED : self.pushSetUpStreamAllIfNedded,
+
+            GitCommitter.STATUS_ALL : self.statusAll,
+            GitCommitter.BRANCH_ALL : self.branchAll,
+            GitCommitter.PULL_ALL : self.pullAll,
+            GitCommitter.CHECKOUT_ALL : self.checkoutAll,
+            GitCommitter.ADD_ALL : self.addAll,
+            GitCommitter.COMMIT_ALL : self.commitAll,
+            GitCommitter.PUSH_ALL : self.pushAll,
+            GitCommitter.ADD_COMMIT_PUSH_ALL : self.addCommitPushAll,
+            GitCommitter.MERGE_ORIGIN_ALL : self.mergeOriginAll,
+
+            GitCommitter.ADD_ENVIRONMENT_VARIABLE : self.addEnvironmentVariable
+        }
+        self.voiceAssistant = VoiceAssistant.VoiceAssistant(self.globals)
 
     def runCommandList(self,commandList):
         globals = self.globals
@@ -77,7 +74,7 @@ class GitCommitter:
                     processPath = f'{globals.localPath}{globals.apisRoot}{apiName}'
                     returnSet[apiName][command] = subprocess.run(command,shell=True,capture_output=True,cwd=processPath)
                     print(self.getProcessReturnValue(returnSet[apiName][command]))
-                    # self.globals.debug(returnSet[apiName][command])
+                    # globals.debug(returnSet[apiName][command])
             except Exception as exception :
                 print(f'{self.globals.ERROR}{apiName}{globals.SPACE_DASH_SPACE}{command}{globals.NEW_LINE}{str(exception)}')
         return returnSet
@@ -100,29 +97,6 @@ class GitCommitter:
                 print(f'{self.globals.ERROR}{apiName}{globals.SPACE_DASH_SPACE}{command}{globals.NEW_LINE}{str(exception)}')
         return returnSet[apiName][command]
 
-    def __init__(self,globals):
-        self.GIT_COMMITTER = globals.GIT_COMMITTER
-        self.globals = globals
-        self.gitUrl = globals.getApiSetting(f'api.git.url')
-        self.gitExtension = globals.getApiSetting(f'api.git.extension')
-        self.commandSet = {
-            COMMAND_ADD_ENVIRONMENT_VARIABLE : self.addEnvironmentVariable,
-
-            COMMAND_CLONE_ALL_IF_NEEDED : self.cloneAllIfNeeded,
-            COMMAND_CHECKOUT_B_ALL_IF_NEEDED : self.checkoutBAllIfNeeded,
-            COMMAND_PUSH_SET_UPSTREAM_ORIGIN_BRANCH_IF_NEEDED : self.pushSetUpStreamAllIfNedded,
-
-            COMMAND_STATUS_ALL : self.statusAll,
-            COMMAND_BRANCH_ALL : self.branchAll,
-            COMMAND_PULL_ALL : self.pullAll,
-            COMMAND_CHECKOUT_ALL : self.checkoutAll,
-            COMMAND_ADD_ALL : self.addAll,
-            COMMAND_COMMIT_ALL : self.commitAll,
-            COMMAND_PUSH_ALL : self.pushAll,
-            COMMAND_ADD_COMMIT_PUSH_ALL : self.addCommitPushAll,
-            COMMAND_MERGE_ORIGIN_ALL : self.mergeOriginAll
-        }
-
     def cloneAllIfNeeded(self,sysCommandList):
         globals = self.globals
         repositoryNameList = list(globals.getPathTreeFromPath(f'{globals.localPath}{globals.apisRoot}').keys())
@@ -131,7 +105,7 @@ class GitCommitter:
             for apiName in globals.apiNameList :
                 if apiName not in repositoryNameList :
                     repositoryUrl = f'{self.gitUrl}{apiName}.{self.gitExtension}'
-                    command = Command.CLONE.replace(Command.TOKEN_REPOSITORY_URL,repositoryUrl)
+                    command = GitCommand.CLONE.replace(GitCommand.TOKEN_REPOSITORY_URL,repositoryUrl)
                     processPath = f'{globals.localPath}{globals.apisRoot}'
                     apiNameCommandListTree[apiName] = [command]
             self.runApiNameCommandListTree(apiNameCommandListTree,path=processPath)
@@ -139,85 +113,89 @@ class GitCommitter:
     def checkoutBAllIfNeeded(self,sysCommandList):
         branchName = self.getArg(GitCommitter._1_ARGUMENT_INDEX,'Branch name',sysCommandList)
         if branchName :
-            commandCheckoutAll = Command.CHECKOUT.replace(Command.TOKEN_BRANCH_NAME,branchName)
-            returnSet = self.runCommandList([Command.FETCH,commandCheckoutAll])
+            commandCheckoutAll = GitCommand.CHECKOUT.replace(GitCommand.TOKEN_BRANCH_NAME,branchName)
+            returnSet = self.runCommandList([commandCheckoutAll])
             if returnSet and returnSet.items():
                 for apiName,specificReturnSet in returnSet.items() :
                     if specificReturnSet and specificReturnSet.items() :
                         for key,value in specificReturnSet.items() :
                             if 'error' in self.getProcessReturnErrorValue(value) :
-                                command = Command.CHECKOUT_DASH_B.replace(Command.TOKEN_BRANCH_NAME,branchName)
+                                command = GitCommand.CHECKOUT_DASH_B.replace(GitCommand.TOKEN_BRANCH_NAME,branchName)
                                 apiNameCommandListTree = {apiName:[command]}
                                 returnCorrectionSet = self.runApiNameCommandListTree(apiNameCommandListTree)
             self.debugReturnSet('checkoutBAllIfNeeded',self.getReturnSetValue(returnSet))
 
     def pushSetUpStreamAllIfNedded(self,sysCommandList):
-        returnSet = self.runCommandList([Command.PUSH])
+        returnSet = self.runCommandList([GitCommand.PUSH])
         if returnSet and returnSet.items():
             for apiName,specificReturnSet in returnSet.items() :
                 if specificReturnSet and specificReturnSet.items() :
                     for key,value in specificReturnSet.items() :
                         for line in self.getProcessReturnErrorValue(value).split(self.globals.NEW_LINE) :
-                            if Command.PUSH_SET_UPSTREAM_ORIGIN in line :
-                                commandPush = Command.BRANCH
+                            if GitCommand.PUSH_SET_UPSTREAM_ORIGIN in line :
+                                commandPush = GitCommand.BRANCH
                                 returnCorrectionSet[apiName][command] = self.runApiNameCommandListTree({apiName:[commandPush]})
                                 returnedValue = self.getProcessReturnValue(returnCorrectionSet[apiName][command]).split(self.globals.NEW_LINE)
                                 if returnedValue :
                                     if '*' in dirtyBranchName :
                                         branchName = dirtyBranchName.split()[1].strip()
-                                        commandPushSetUpStreamAll = Command.PUSH_SET_UPSTREAM_ORIGIN_BRANCH.replace(Command.TOKEN_BRANCH_NAME,branchName)
+                                        commandPushSetUpStreamAll = GitCommand.PUSH_SET_UPSTREAM_ORIGIN_BRANCH.replace(GitCommand.TOKEN_BRANCH_NAME,branchName)
                                         returnCorrectionSet[apiName][commandPushSetUpStreamAll] = self.runApiNameCommandListTree({apiName:[commandPushSetUpStreamAll]})
         self.debugReturnSet('pushSetUpStreamAllIfNedded',self.getReturnSetValue(returnSet))
 
     def statusAll(self,sysCommandList):
-        returnSet = self.runCommandList([Command.STATUS])
+        returnSet = self.runCommandList([GitCommand.STATUS])
         self.debugReturnSet('statusAll',self.getReturnSetValue(returnSet))
 
     def branchAll(self,sysCommandList):
-        returnSet = self.runCommandList([Command.BRANCH])
+        returnSet = self.runCommandList([GitCommand.BRANCH])
         self.debugReturnSet('branchAll',self.getReturnSetValue(returnSet))
 
+    def fetchAll(self,sysCommandList):
+        returnSet = self.runCommandList([GitCommand.FETCH])
+        self.debugReturnSet('fetchAll',self.getReturnSetValue(returnSet))
+
     def pullAll(self,sysCommandList):
-        returnSet = self.runCommandList([Command.PULL])
+        returnSet = self.runCommandList([GitCommand.PULL])
         self.debugReturnSet('pullAll',self.getReturnSetValue(returnSet))
 
     def checkoutAll(self,sysCommandList):
         branchName = self.getArg(GitCommitter._1_ARGUMENT_INDEX,'Branch name',sysCommandList)
         if branchName :
-            commandCheckoutAll = Command.CHECKOUT.replace(Command.TOKEN_BRANCH_NAME,branchName)
+            commandCheckoutAll = GitCommand.CHECKOUT.replace(GitCommand.TOKEN_BRANCH_NAME,branchName)
             returnSet = self.runCommandList([commandCheckoutAll])
             self.debugReturnSet('checkoutAll',self.getReturnSetValue(returnSet))
 
     def addAll(self,sysCommandList):
-        returnSet = self.runCommandList([Command.ADD])
+        returnSet = self.runCommandList([GitCommand.ADD])
         self.debugReturnSet('addAll',self.getReturnSetValue(returnSet))
 
     def commitAll(self,sysCommandList):
         commitMessage = self.getArg(GitCommitter._1_ARGUMENT_INDEX,'Commit message',sysCommandList)
         if commitMessage :
-            commandCommit = Command.COMMIT.replace(Command.TOKEN_COMMIT_MESSAGE,commitMessage)
+            commandCommit = GitCommand.COMMIT.replace(GitCommand.TOKEN_COMMIT_MESSAGE,commitMessage)
             returnSet = self.runCommandList([commandCommit])
             self.debugReturnSet('commitAll',self.getReturnSetValue(returnSet))
 
     def pushAll(self,sysCommandList):
-        returnSet = self.runCommandList([Command.PUSH])
+        returnSet = self.runCommandList([GitCommand.PUSH])
         self.debugReturnSet('pushAll',self.getReturnSetValue(returnSet))
 
     def addCommitPushAll(self,sysCommandList):
         commitMessage = self.getArg(GitCommitter._1_ARGUMENT_INDEX,'Commit message',sysCommandList)
         if commitMessage :
-            commandCommit = Command.COMMIT.replace(Command.TOKEN_COMMIT_MESSAGE,commitMessage)
+            commandCommit = GitCommand.COMMIT.replace(GitCommand.TOKEN_COMMIT_MESSAGE,commitMessage)
             returnSet = self.runCommandList([
-                Command.ADD,
+                GitCommand.ADD,
                 commandCommit,
-                Command.PUSH
+                GitCommand.PUSH
             ])
             self.debugReturnSet('addCommitPushAll',self.getReturnSetValue(returnSet))
 
     def mergeOriginAll(self,sysCommandList):
         branchName = self.getArg(GitCommitter._1_ARGUMENT_INDEX,'Branch name',sysCommandList)
         if branchName :
-            commandMergeOriginAll = Command.MERGE_ORIGIN.replace(Command.TOKEN_BRANCH_NAME,branchName)
+            commandMergeOriginAll = GitCommand.MERGE_ORIGIN.replace(GitCommand.TOKEN_BRANCH_NAME,branchName)
             returnSet = self.runCommandList([commandMergeOriginAll])
             self.debugReturnSet('mergeOriginAll',self.getReturnSetValue(returnSet))
 
@@ -226,28 +204,30 @@ class GitCommitter:
         variableValue = self.getArg(GitCommitter._2_ARGUMENT_INDEX,'Environment variable value',sysCommandList)
         if variableKey and variableValue :
             globals = self.globals
-            if variableKey == Command.KW_SELF :
+            if variableKey == GitCommand.KW_SELF :
                 variableValue = f'{globals.localPath}{globals.apisRoot}{GitCommitter.__name__}{globals.BACK_SLASH}{globals.baseApiPath}'
                 print(variableValue)
             os.environ[variableKey] = variableValue
 
+    def wakeUpVoiceAssistant(self,sysCommandList):
+        WakeUpVoiceAssistant.run(self)
+
     def handleSystemCommand(self,sysCommandList):
         sysCommandList = sysCommandList.copy()
         globals = self.globals
-        MISSING_SPACE = 'Missing '
-        if len(sysCommandList) == 0 or globals.GIT_COMMITTER not in sysCommandList :
-            print(f'{globals.ERROR}{MISSING_SPACE}{globals.DOUBLE_QUOTE}{globals.GIT_COMMITTER}{globals.DOUBLE_QUOTE}')
+        if len(sysCommandList) == 0 or globals.GITC_GIT_COMMITTER not in sysCommandList :
+            print(f'{globals.ERROR}{GitCommitter.MISSING_SPACE}{globals.DOUBLE_QUOTE}{globals.GITC_GIT_COMMITTER}{globals.DOUBLE_QUOTE}')
             return
-        if len(sysCommandList) < self.COMMAND_INDEX :
-            print(f'{globals.ERROR}{MISSING_SPACE}{globals.GIT_COMMITTER} command')
+        if len(sysCommandList) < GitCommitter.COMMAND_INDEX :
+            print(f'{globals.ERROR}{GitCommitter.MISSING_SPACE}{globals.GITC_GIT_COMMITTER} command')
             return
-        gitCommiterCallCommand = sysCommandList[self.GIT_COMMITTER_INDEX]
-        command = sysCommandList[self.COMMAND_INDEX]
-        if globals.GIT_COMMITTER == gitCommiterCallCommand :
+        gitCommiterCallCommand = sysCommandList[GitCommitter.GIT_COMMITTER_INDEX]
+        command = sysCommandList[GitCommitter.COMMAND_INDEX]
+        if globals.GITC_GIT_COMMITTER == gitCommiterCallCommand :
             try :
                 self.commandSet[command](sysCommandList)
-            except :
-                print(f'{globals.ERROR}command not fount')
+            except Exception as exception :
+                print(f'''{globals.ERROR}processing command "{gitCommiterCallCommand} {command}": {str(exception)}''')
 
     def getProcessReturnValue(self,processReturn):
         return str(processReturn.stdout,self.globals.ENCODING)
@@ -285,7 +265,7 @@ class GitCommitter:
             return self.validInput(self.getImput(typingGetMessage))
 
     def validInput(self,input):
-        if input == COMMAND_SKIP :
+        if input == GitCommitter.SKIP :
             return None
         else :
             return input
---
+++ api/src/domain/WakeUpVoiceAssistant.py
@@ -0,0 +1,33 @@
+import Levenshtein
+
+import VoiceAssistant
+
+VoiceAssistant = VoiceAssistant.VoiceAssistant
+
+def run(gitCommitter) :
+    globals = gitCommitter.globals
+    voiceAssistant = gitCommitter.voiceAssistant
+    voiceAssistant.awake = True
+    globals = voiceAssistant.globals
+
+    selectedCommand = None
+    while voiceAssistant.awake :
+        content = voiceAssistant.listen()
+        if content not in VoiceAssistant.SLEEP :
+            if selectedCommand and content in gitCommitter.CONFIRM :
+                gitCommitter.handleSystemCommand([globals.GITC_GIT_COMMITTER,selectedCommand[0][1][0]])
+            else :
+                selectedCommand = None
+                voiceAssistant.speak(content)
+                gradedCommandSet = {}
+                for command in gitCommitter.commandSet.keys() :
+                    commandScore = Levenshtein.distance(content,globals.SPACE.join(command.split(globals.DASH)))
+                    if gradedCommandSet.get(commandScore) :
+                        gradedCommandSet[commandScore].append(command)
+                    else :
+                        gradedCommandSet[commandScore] = [command]
+                selectedCommand = sorted(gradedCommandSet.items())
+                globals.debug(f'{globals.GITC_GIT_COMMITTER} {selectedCommand[0][1][0]}')
+        elif content in VoiceAssistant.SLEEP :
+            voiceAssistant.awake = False
+    return selectedCommand
